ac895dbff76eaa57d65d15749e2fb5c1c9bcc804
Andrey <headshotik62@gmail.com>
2016-09-27 20:15:05 +0300

    add board page

---
+++ resources/views/user/pages/board.blade.php
@@ -0,0 +1,52 @@
+@extends('user.main')
+
+@section('content')
+    <section id="main-content" class="reloading">
+        <section class="wrapper">
+
+            <div class="row">
+                <div class="col-sm-9">
+                    <div class="row">
+                        Lorem ipsum dolor sit amet,
+                        consectetur adipisicing elit. Assu
+                        menda deserunt doloribus ducimus, eligen
+                        di eveniet explicabo illum incidunt ipsum necessi
+                        tatibus nihil sed, sit tempora, totam ullam vel veniam
+                        voluptas voluptatibus. Quaerat!
+
+                    </div>
+                    <div class="row">
+
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    123
+                </div>
+            </div>
+            <div class="row mt">
+                <div class="col-sm-9">
+                    <div class="info-block"></div>
+                    <div class="row mt">
+                        <div class="col-sm-6">
+                            <div class="info-block">
+
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="info-block">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="info-bock">
+
+                </div>
+            </div>
+            </div>
+
+        </section>
+    </section>
+@stop
