291661f06193f85a35930ce6b369bfd37ec27133
Andrey <headshotik62@gmail.com>
2016-09-29 21:58:44 +0300

    rapapap

--- resources/views/user/forms/feedback/tehForm.blade.php
+++ resources/views/user/forms/feedback/tehForm.blade.php
@@ -1,29 +1,39 @@
-<div class="col-sm-12  text-st">
-    <form class="form-horizontal form-pricing " role="form">
+<div class="col-sm-12  text-st form-pricing">
+    <form class="form-horizontal  " role="form">
         <div class="col-sm-10 col-sm-offset-1">
-            <label class="col-sm-4 control-label text-c">Ваш e-mail</label>
-            <div class="col-sm-6 col-sm-offset-1">
-                <input name="email" type="email" class="form-control" title="Ссылка на контент" placeholder="Ваш e-mail">
+            <div class="row">
+                <label class="col-sm-4 control-label text-c">Ваш e-mail</label>
+                <div class="col-sm-6 col-sm-offset-1">
+                    <input name="email" type="email" class="form-control" title="Ссылка на контент" placeholder="Ваш e-mail">
+                </div>
             </div>
-            <label class="col-sm-4 control-label text-c">Выбирете приоритет формы</label>
-            <div class="col-sm-6 col-sm-offset-1">
-                <select name="priority" class="form-control">
-                    <option value="1">Низкий</option>
-                    <option value="2">Средний</option>
-                    <option value="3" disabled>Высокий</option>
-                </select>
+            <div class="row mt">
+                <label class="col-sm-4 control-label text-c">Выбирете приоритет формы</label>
+                <div class="col-sm-6 col-sm-offset-1">
+                    <select name="priority" class="form-control">
+                        <option value="1">Низкий</option>
+                        <option value="2">Средний</option>
+                        <option value="3" disabled>Высокий</option>
+                    </select>
+                </div>
             </div>
-            <label class="col-sm-4 control-label text-c">Тема</label>
-            <div class="col-sm-6 col-sm-offset-1">
-                <input type="text" class="form-control" name="theme" title="Ссылка на контент" placeholder="Тема">
+            <div class="row mt">
+                <label class="col-sm-4 control-label text-c">Тема</label>
+                <div class="col-sm-6 col-sm-offset-1">
+                    <input type="text" class="form-control" name="theme" title="Ссылка на контент" placeholder="Тема">
+                </div>
             </div>
-            <label class="col-sm-4 control-label text-c">Текст</label>
-            <div class="col-sm-6 col-sm-offset-1">
-                <textarea rows="5" class="form-control" name="text" title="Ссылка на контент" placeholder="Текст"></textarea>
+            <div class="row mt">
+                <label class="col-sm-4 control-label text-c">Текст</label>
+                <div class="col-sm-6 col-sm-offset-1">
+                    <textarea rows="5" class="form-control" name="text" title="Ссылка на контент" placeholder="Текст"></textarea>
+                </div>
             </div>
-            <div class="col-sm-6 col-sm-offset-1 mt ">
-                <div class="col-md-offset-9 col-md-3">
-                    <button type="submit" class="btn btn-primary btn-lg" value="go">Отправить</button>
+            <div class="row mt">
+                <div class="col-sm-6 col-sm-offset-1 mt ">
+                    <div class="col-md-offset-9 col-md-3">
+                        <button type="submit" class="btn btn-primary btn-lg" value="go">Отправить</button>
+                    </div>
                 </div>
             </div>
         </div>
